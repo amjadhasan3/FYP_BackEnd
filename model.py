802b9d2ac957f0c716a97724f643110b2d73985f
Amjad Hasan <124026525+amjadhasan3@users.noreply.github.com>
2025-03-26 10:18:05 +0530

    renamed dataset

--- model.py
+++ model.py
@@ -73,7 +73,7 @@ def prepare_features(df):
     return X, y
 
 def run_stacked_model():
-    df = pd.read_csv("output.csv")
+    df = pd.read_csv("movie_dataset.csv")
     X, y = prepare_features(df)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
